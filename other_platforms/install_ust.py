51e6ea3ca5a3872c3ff4386713c8fdf7e7084fa0
Unknown <danimaejanssen@gmail.com>
2019-02-12 17:40:29 -0600

    touchups

--- other_platforms/install_ust.py
+++ other_platforms/install_ust.py
@@ -36,7 +36,6 @@ class web_util:
 
     def fetch_resources(self, config):
 
-        config['resources'] = {}
         config['resources']['examples_url'] = "https://github.com/adobe-apiplatform/user-sync.py/releases/download/v2.4/examples.tar.gz"
 
         if  config['host_key'] == "ubuntu":
@@ -83,7 +82,28 @@ class bash_util:
 class main:
 
     def __init__(self):
+
+        platform_details = platform.linux_distribution()
+        self.logger = LoggingContext().init_logger(platform_details)
+
         self.config = {}
+        self.config['resources'] = {}
+        self.config['ust_directory'] = "adobe-user-sync-tool"
+        self.config['host_platform'] = platform_details[0]
+        self.config['host_name'] = platform_details[2]
+        self.config['host_version'] = platform_details[1]
+
+        if (bool(re.search("(ubuntu)", self.config['host_platform'], re.I))):
+            self.config['host_key'] = "ubuntu"
+        elif (bool(re.search("(cent)|(fedora)|(red)", self.config['host_platform'], re.I))):
+            self.config['host_key'] = "centos"
+
+        self.web = web_util()
+        self.bash = bash_util(self.config['host_key'])
+        self.config['python2_version'] = self.bash.get_python_version(2)
+        self.config['python3_version'] = self.bash.get_python_version(3)
+        self.config['python27'] = self.config['python2_version'].startswith("2.7")
+        self.config['python36'] = self.config['python3_version'].startswith("3.6")
 
     def create_shell(self, filename, command):
         text_file = open(filename, "w")
@@ -109,35 +129,11 @@ class main:
 
         self.bash.shell("sudo chmod 777 -R " + ust_dir)
 
-    def initialize(self):
-        platform_details = platform.linux_distribution()
-        self.config['ust_directory'] = "adobe-user-sync-tool"
-        self.config['host_platform'] = platform_details[0]
-        self.config['host_name'] = platform_details[2]
-        self.config['host_version'] = platform_details[1]
-
-        if (bool(re.search("(ubuntu)", self.config['host_platform'], re.I))):
-            self.config['host_key'] = "ubuntu"
-        elif (bool(re.search("(cent)|(fedora)|(red)", self.config['host_platform'], re.I))):
-            self.config['host_key'] = "centos"
-
-        self.logger = LoggingContext().init_logger(self.config)
-
-        self.bash = bash_util(self.config['host_key'])
-        self.web = web_util()
+    def run(self):
 
         shutil.rmtree(self.config['ust_directory'], ignore_errors=True)
         os.mkdir(self.config['ust_directory'])
-
-    def run(self):
-        self.initialize()
-        self.config['python2_version'] = self.bash.get_python_version(2)
-        self.config['python3_version'] = self.bash.get_python_version(3)
-        self.config['python27'] = self.config['python2_version'].startswith("2.7")
-        self.config['python36'] = self.config['python3_version'].startswith("3.6")
-
         self.web.fetch_resources(self.config)
-
         self.bash.install_preRequisites(self.config)
         self.install_ust()
 
@@ -146,9 +142,9 @@ class main:
 
 class LoggingContext:
 
-    def init_logger(self, config):
+    def init_logger(self, platform):
         log_params = {
-            'this_name': config['host_platform'] + " " + config['host_version'] + " " + config['host_name'],
+            'this_name': platform[0] + " " + platform[1] + " " + platform[2],
             'log_level': logging.getLevelName("DEBUG"),
             'date_fmt': '%Y-%m-%d %H:%M:%S'
         }
