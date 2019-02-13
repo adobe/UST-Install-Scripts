c2e667fdc7063d86028fc59cf51e091b78a670cf
Unknown <danimaejanssen@gmail.com>
2019-02-13 17:42:00 -0600

    Much progress, ubuntu mostly finished
    Removed requests in favor of urllib

--- other_platforms/install_ust.py
+++ other_platforms/install_ust.py
@@ -7,28 +7,50 @@ import os
 import tarfile
 import shutil
 
+
+
 try:
-    import requests
+    from urllib.request import urlretrieve
 except ImportError:
-    os.system("sudo pip install requests")
-    import requests
+    from urllib import urlretrieve
 
 meta = {}
 meta['ubuntu'] = {
-    'install_cmd':'sudo apt-get --force-yes -y install',
-    'update_cmd': 'sudo apt-get update',
-    'openssl_reqs': ['openssl', 'libssl-dev']
-}
 
+    'update_cmd': 'sudo apt-get update',
+    'openssl_script': ['sudo apt-get -y install openssl',
+                       'sudo apt-get -y install libssl-dev'],
+    '16': {
+        'python_vers': '2.7',
+        'python_inst': ['sudo apt-get -y install libpython2.7',
+                        'sudo apt-get -y install libatlas3-base',
+                        'sudo apt-get -y install python2.7'],
+    },
+    '17': {
+        'python_vers': '2.7',
+        'python_inst': [
+            'sudo sed -i -e "s/archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g" /etc/apt/sources.list',
+            'sudo apt-get update',
+            'sudo apt-get -y install libpython2.7',
+            'sudo apt-get -y install libatlas3-base',
+            'sudo apt-get -y install python2.7'],
+    },
+    '18': {
+        'python_vers': '3.6',
+        'python_inst': ['sudo apt-get -y install python3.6'],
+    }
+}
 
 
 class web_util:
 
+    def __init__(self, logger):
+        self.logger = logger
+
     def download(self, url, dir):
 
-        r = requests.get(url)
         filename = dir + os.sep + str(url.rpartition('/')[2])
-        with open(filename, 'wb') as f: f.write(r.content)
+        urlretrieve(url, filename)
 
         if (filename.endswith(".tar.gz")):
             tarfile.open(filename).extractall(path=dir)
@@ -37,47 +59,45 @@ class web_util:
     def fetch_resources(self, config):
 
         config['resources']['examples_url'] = "https://github.com/adobe-apiplatform/user-sync.py/releases/download/v2.4/examples.tar.gz"
+        config['resources']['ust_url'] = "https://github.com/adobe-apiplatform/user-sync.py/releases/download/v2.4/user-sync-v2.4-"
 
-        if  config['host_key'] == "ubuntu":
-            config['resources']['ust_py3_url'] = "https://github.com/adobe-apiplatform/user-sync.py/releases/download/v2.4/user-sync-v2.4-ubuntu1604-py367.tar.gz"
-            config['resources']['ust_py2_url'] = "https://github.com/adobe-apiplatform/user-sync.py/releases/download/v2.4/user-sync-v2.4-ubuntu1604-py2715.tar.gz"
+        isPy3 = config['python']['target']['python_vers'].startswith("3")
 
+        if   config['platform']['host_key'] == "ubuntu":
+             config['resources']['ust_url'] += "ubuntu1604-py367.tar.gz" if isPy3 else "ubuntu1604-py2715.tar.gz"
 
-        elif config['host_key'] == "centos":
-            config['resources']['ust_py3_url'] = "https://github.com/adobe-apiplatform/user-sync.py/releases/download/v2.4/user-sync-v2.4-centos7-py367.tar.gz"
-            config['resources']['ust_py2_url'] = "https://github.com/adobe-apiplatform/user-sync.py/releases/download/v2.4/user-sync-v2.4-centos7-py275.tar.gz"
-
+        elif config['platform']['host_key'] == "centos":
+             config['resources']['ust_url'] += "centos7-py367.tar.gz" if isPy3 else "centos7-py275.tar.gz"
 
 class bash_util:
 
-
-    def __init__(self, host_key):
-        self.meta = meta[host_key]
-        self.install_cmd = self.meta['install_cmd']
-        self.update_cmd = self.meta['update_cmd']
+    def __init__(self, logger):
+        self.logger = logger
 
 
     def shell(self, cmd):
         out = subprocess.check_output(cmd + " 2>&1", shell=True)
-        print(out)
+        print(out.decode())
         return out
 
     def get_python_version(self, major):
-        return re.search("\d.*", self.shell("python" + str(major) + " -V")).group()
+        try:
+            return re.search("\d.*", self.shell("python" + str(major) + " -V")).group()
+        except:
+            return "[none]"
 
-    def install_package(self, name):
-        self.shell(self.install_cmd + " " + name)
+    def install_dependencies(self, config):
 
-    def install_preRequisites(self, config):
+        self.logger.info("Update package repositories... ")
+        self.shell(config['update_cmd'])
 
-        for p in self.meta['openssl_reqs']:
-            self.install_package(p)
+        self.logger.info("Installing openSSL...")
+        for c in config['openssl']:
+            self.shell(c)
 
-
-
-        print
-
-
+        self.logger.info("Installing Python...")
+        for c in config['python']['target']['python_inst']:
+            self.shell(c)
 
 class main:
 
@@ -86,24 +106,36 @@ class main:
         platform_details = platform.linux_distribution()
         self.logger = LoggingContext().init_logger(platform_details)
 
-        self.config = {}
-        self.config['resources'] = {}
+        self.config = {'platform':{}, 'python':{}, 'resources':{}}
+        self.web = web_util(self.logger)
+        self.bash = bash_util(self.logger)
+        py2_version = self.bash.get_python_version(2)
+        py3_version = self.bash.get_python_version(3)
+
+        if (bool(re.search("(ubuntu)", platform_details[0], re.I))):
+            self.config['platform']['host_key'] = "ubuntu"
+        elif (bool(re.search("(cent)|(fedora)|(red)", platform_details[0], re.I))):
+            self.config['platform']['host_key'] = "centos"
+
+        self.config['platform']['host_platform'] = platform_details[0]
+        self.config['platform']['host_name'] = platform_details[2]
+        self.config['platform']['host_version'] = platform_details[1]
+        self.config['platform']['major_version'] = platform_details[1][:2]
         self.config['ust_directory'] = "adobe-user-sync-tool"
-        self.config['host_platform'] = platform_details[0]
-        self.config['host_name'] = platform_details[2]
-        self.config['host_version'] = platform_details[1]
+        self.config['update_cmd'] = meta[self.config['platform']['host_key']]['update_cmd']
+        self.config['openssl'] = meta[self.config['platform']['host_key']]['openssl_script']
+        self.config['python'] = {
+            'target': meta[self.config['platform']['host_key']][self.config['platform']['major_version']],
+            'current': {
+                'python2_version':py2_version,
+                'python3_version':py3_version,
+                'python2_status': py2_version.startswith("2.7"),
+                'python3_status': py3_version.startswith("3.6")
+            }
+        }
 
-        if (bool(re.search("(ubuntu)", self.config['host_platform'], re.I))):
-            self.config['host_key'] = "ubuntu"
-        elif (bool(re.search("(cent)|(fedora)|(red)", self.config['host_platform'], re.I))):
-            self.config['host_key'] = "centos"
+        print
 
-        self.web = web_util()
-        self.bash = bash_util(self.config['host_key'])
-        self.config['python2_version'] = self.bash.get_python_version(2)
-        self.config['python3_version'] = self.bash.get_python_version(3)
-        self.config['python27'] = self.config['python2_version'].startswith("2.7")
-        self.config['python36'] = self.config['python3_version'].startswith("3.6")
 
     def create_shell(self, filename, command):
         text_file = open(filename, "w")
@@ -116,7 +148,7 @@ class main:
         conf_dir = os.path.join(ust_dir,'examples','config files - basic')
 
         self.web.download(self.config['resources']['examples_url'], ust_dir)
-        #self.web.download(self.config['resources']['ust_py2_url'], ust_dir)
+        self.web.download(self.config['resources']['ust_url'], ust_dir)
 
         shutil.copy(os.path.join(conf_dir,"connector-ldap.yml"), ust_dir)
         shutil.copy(os.path.join(conf_dir,"connector-umapi.yml"), ust_dir)
@@ -134,8 +166,8 @@ class main:
         shutil.rmtree(self.config['ust_directory'], ignore_errors=True)
         os.mkdir(self.config['ust_directory'])
         self.web.fetch_resources(self.config)
-        self.bash.install_preRequisites(self.config)
         self.install_ust()
+        self.bash.install_dependencies(self.config)
 
         print
 
@@ -152,7 +184,7 @@ class LoggingContext:
         f_format = "%(asctime)s " + log_params['this_name'] + "  [%(name)-8.8s]  [%(levelname)-5.5s]  :::  %(message)s"
         logging.basicConfig(level=log_params['log_level'], format=f_format, datefmt=log_params['date_fmt'])
         logger = logging.getLogger("main")
-        f_handler = logging.FileHandler('pyinstall.log', 'w')
+        f_handler = logging.FileHandler('ust_install.log', 'w')
         f_handler.setFormatter(logging.Formatter(f_format, log_params['date_fmt']))
         f_handler.setLevel(log_params['log_level'])
         logger.addHandler(f_handler)
@@ -172,15 +204,3 @@ class LoggingContext:
 
 
 main().run()
-
-# import gzip
-# from cStringIO import StringIO
-# tar = tarfile.open(mode="r:gz", fileobj=StringIO(r.content))
-#
-# #   tar = tarfile.open("sample.tar.gz")
-# for entry in tar.getnames():  # list each entry one by one
-#     fileobj = tar.extractfile(entry)
-#     with open(entry, 'wb') as f:
-#         f.write(fileobj.read())
-#
-#     print
